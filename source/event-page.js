f1e5a12eb34b165cc7a0a8b99ab76b336358fe54
Paul M Furley <paul@paulfurley.com>
2017-04-11 10:36:43 +0100

    Event page: respond to open dialog message
    - create a new inactive tab with the dialog box
    - create a new window, and inject the tab into it
    - send the new tab/window a message referring to the UUID and Gmail tab
      ID so it can store it for later.

--- source/event-page.js
+++ source/event-page.js
@@ -11,16 +11,41 @@ chrome.browserAction.onClicked.addListener(function(tab) {
 });
 
 chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log(sender.tab ?
-                "from a content script:" + sender.tab.url :
-                "from the extension");
-    if (request.greeting == "hello")
+  function(messageFromGmail, sender, sendResponseToGmailContentScript) {
+    console.debug("event page got message: ", messageFromGmail);
+
+    if (messageFromGmail.messageType == "openSecureSendDialog")
+      var gmailTabId = sender.tab.id;
       var width = 615;
       var height = 400;
       var left = Math.round((screen.width/2)-(width/2));
-      var top = Math.round((screen.height/2)-(height/2));
-      chrome.windows.create({ url: "options.html", type: "popup", width: width, height: height, left: left, top: top });
-      sendResponse({farewell: "goodbye"});
+      var _top = Math.round((screen.height/2)-(height/2));
+
+      chrome.tabs.create({
+          url: chrome.extension.getURL('options.html'),
+          active: false
+        }, function(tab) {
+            // After the tab has been created, open a window to inject the tab
+            chrome.windows.create({
+                tabId: tab.id,
+                type: 'popup',
+                focused: true,
+                width: width,
+                height: height,
+                left: left,
+                top: _top
+            }, function(createdWindow) {
+              console.log("Created new window: ", createdWindow);
+
+              chrome.tabs.sendMessage(
+                tab.id,
+                {
+                  messageType: "setInputElementUUID",
+                  gmailTabId: gmailTabId,
+                  uuid: messageFromGmail.inputElementUUID
+                }
+              );
+            });
+        });
   }
 );
