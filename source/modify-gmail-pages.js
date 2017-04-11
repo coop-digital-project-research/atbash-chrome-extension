6d53da99dd370c83a7ca9d4eee6ae9dc3f6dd2a6
Paul M Furley <paul@paulfurley.com>
2017-04-11 10:35:22 +0100

    Trigger open dialog from Gmail content script
    Modify the DOM to add in unique UUIDs for the input, in order that we
    can later retrieve these to inject the secure URL.

--- source/modify-gmail-pages.js
+++ source/modify-gmail-pages.js
@@ -36,13 +36,57 @@ function cloneAttachmentButton(buttonContainer) {
 function createSafeSendButtonFromAttachmentButton(existingAttachmentButton) {
   console.log("Cloning existing attachment button", existingAttachmentButton);
   var safeSendButton = existingAttachmentButton.cloneNode(true);
+  var relatedInputArea = getRelatedInputElement(existingAttachmentButton);
 
   safeSendButton.style.backgroundColor = "green";
   safeSendButton.setAttribute('data-tooltip', "Send files securely");
 
+  addDataRelatedInputUUIDAttributeToNodeAndChildren(safeSendButton, relatedInputArea.getAttribute('data-uuid'));
+
   addClass(safeSendButton, safeSendButtonClass);
   safeSendButton.addEventListener('click', triggerSecureSendDialog, false);
   return safeSendButton;
+}
+
+function addDataRelatedInputUUIDAttributeToNodeAndChildren(element, uuid) {
+  element.setAttribute('data-related-input-uuid', uuid);
+
+  for(var i = 0 ; i < element.children.length ; i++) {
+    addDataRelatedInputUUIDAttributeToNodeAndChildren(element.children[i], uuid);
+  }
+}
+
+
+function getRelatedInputElement(attachmentButton) {
+  var enclosingTable = getElementsByXPath(
+    './/ancestor::table[contains(@class, "aoP")]',
+    parent=attachmentButton
+  )[0];
+
+  console.log("enclosingTable: ", enclosingTable);
+
+  var composeTextInput = enclosingTable && enclosingTable.getElementsByClassName("editable")[0];
+
+  console.log("composeTextInput: ", composeTextInput);
+
+  if(composeTextInput) {
+    var uuid = guid();
+    console.debug("Adding UUID ", uuid, " to element ", composeTextInput);
+    composeTextInput.setAttribute("data-uuid", uuid);
+    return composeTextInput;
+  } else {
+    console.warn("Failed to find related text input box related to attachmentButton: ", attachmentButton);
+  }
+}
+
+function guid() {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+  return s4() + s4() + '-' + s4() + '-' + s4() + '-' +
+    s4() + '-' + s4() + s4() + s4();
 }
 
 function getElementsByXPath(xpath, parent)
@@ -57,6 +101,19 @@ function getElementsByXPath(xpath, parent)
   return results;
 }
 
+function getElementsByAttribute(attribute, context) {
+  var nodeList = (context || document).getElementsByTagName('*');
+  var nodeArray = [];
+  var iterator = 0;
+  var node = null;
+
+  while (node = nodeList[iterator++]) {
+    if (node.hasAttribute(attribute)) nodeArray.push(node);
+  }
+
+  return nodeArray;
+}
+
 function addClass(el, className) {
   if (el.classList) {
     el.classList.add(className);
@@ -68,8 +125,19 @@ function addClass(el, className) {
 
 setInterval(findAndModifyAttachmentButton, 2000);
 
-function triggerSecureSendDialog(){
-  chrome.runtime.sendMessage({greeting: "hello"}, handleSecureSendDialogResponse);
+function triggerSecureSendDialog(event){
+  console.debug("triggerSecureSendDialog got event: ", event);
+  var inputElementUUID = event.target.getAttribute("data-related-input-uuid");
+
+  if(inputElementUUID) {
+    chrome.runtime.sendMessage({
+      messageType: "openSecureSendDialog",
+      inputElementUUID: inputElementUUID,
+    }, handleSecureSendDialogResponse);
+  } else {
+    console.error("triggerSecureSendDialog couldn't get a data-related-input-uuid to send to the new dialog popup.");
+  }
+
 }
 
 function handleSecureSendDialogResponse(response) {
